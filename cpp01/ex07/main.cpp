cac24c591655fc0c3ac7c95a29ea19f30d4c3043
evrard dessain <evrard.dessain@gmail.com>
2021-04-18 16:27:45 +0200

    day01 ex07

--- cpp01/ex07/main.cpp
+++ cpp01/ex07/main.cpp
@@ -1,3 +1,48 @@
 #include <iostream>
 #include <fstream>
 
+int	error(std::string str)
+{
+	std::cout << str << std::endl;
+	return (1);
+}
+
+int     main(int argc, char **argv)
+{
+    if (argc != 4)
+		return (error("Error : arguments not valid"));
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+    std::ifstream   ifs(filename);
+    if (!ifs)
+        return (error("Error : File can't open"));
+    filename += ".replace";
+    std::ofstream ofs(filename);
+    if (!ofs)
+        return (error("Error : File can't open"));
+
+    std::string str;
+    char    c;
+    int     i;
+
+    while (ifs.get(c))
+    {
+        if (c == ' ')
+        {
+            if (str.compare(s1) == 0)
+                ofs << s2 << " ";
+            else
+                ofs << str << " ";
+            str.erase();
+        }
+        else
+            str += c;
+    }
+    if (str.compare(s1) == 0)
+        ofs << s2 << " ";
+    else
+        ofs << str << " ";
+    str.erase();
+    return (0);
+}
