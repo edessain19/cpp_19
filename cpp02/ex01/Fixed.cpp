a9a1aecff5767c0dc5da08c44b23e427a5ad21f7
Evrard Dessain <edessain@fu-r8-p4.s19.be>
2021-04-19 12:37:06 +0200

    Day02 done

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -1,4 +1,3 @@
-
 # include "Fixed.hpp"
 
 /*
@@ -9,6 +8,18 @@
 Fixed::Fixed() : _fixedValue(0)
 {
 	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int i)
+{
+	std::cout << "Int constructor called" << std::endl;
+	_fixedValue = (i << _bitsNbr);
+}
+
+Fixed::Fixed(const float i)
+{
+	std::cout << "Float constructor called" << std::endl;
+	_fixedValue = roundf(i * (1 << _bitsNbr));
 }
 
 // COPY
@@ -51,10 +62,28 @@ int Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
 	return (_fixedValue);
-}
+} 
 
 void Fixed::setRawBits(int const raw)
 {
 	std::cout << "setRawBits member function called" << std::endl;
 	_fixedValue = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+	// By dividing we make an equivalent of fixedValue << bitsNbr
+	return (_fixedValue / (float)(1 << _bitsNbr));
+}
+
+int Fixed::toInt(void) const
+{
+	// We shift the binary point from position fixedValue to position 0
+	return (_fixedValue >> _bitsNbr);
+}
+
+//Goal is to be able to insert into output streams like std::cout, it returns the output stream with its floating point inserted in it.
+std::ostream &operator<<(std::ostream &output_stream, const Fixed &f)
+{
+  return (output_stream << f.toFloat());
 }
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -4,21 +4,30 @@
 
 # include <iostream>
 # include <string>
+# include <stdio.h>
+# include <math.h>
 
 class Fixed
 {
 	public:
 		Fixed();
+		Fixed(const int i);
+		Fixed(const float i);
 		Fixed(Fixed const& copy);
 		~Fixed();
 		Fixed& operator=(Fixed const& copy);
-
+		
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
+		float toFloat(void) const;
+		int toInt(void) const;
+
 	private:
 		int 				_fixedValue;
-		static const int 	_bitsNbr;
+		static const int 	_bitsNbr = 8;
 
 };
+
+std::ostream &operator<<(std::ostream &ostream, const Fixed &f);
 
 #endif
