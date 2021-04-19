b4754973e6390bd5ef0d6832b82ff641ca41e7fd
evrard dessain <evrard.dessain@gmail.com>
2021-04-19 08:26:40 +0200

    day01 ex07

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,46 @@
+
+# include "Fixed.hpp"
+
+/*
+** CONSTRUCTORS
+*/
+
+// DEFAULT
+Fixed::Fixed()
+{
+	
+}
+
+// COPY
+Fixed::Fixed(Fixed const& copy)
+{
+	
+}
+
+/*
+** DESTRUCTOR
+*/
+
+Fixed::~Fixed()
+{
+	
+}
+
+/*
+** OPERATORS
+*/
+
+// ASSIGNATION
+Fixed& Fixed::operator=(Fixed const& copy)
+{
+	if (this != &copy)
+	{
+		
+	}
+	return *this;
+}
+
+/*
+** MEMBER FUNCTIONS
+*/
+
---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,21 @@
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+	public:
+		Fixed();
+		Fixed(Fixed const& copy);
+		~Fixed();
+		Fixed& operator=(Fixed const& copy);
+
+	private:
+
+
+};
+
+#endif
