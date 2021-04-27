533d21543e6b51b5731dff4c7b4012a7c737c5b1
Evrard Dessain <edessain@fu-r8-p4.s19.be>
2021-04-27 12:05:50 +0200

    Day06 ex00

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -1,6 +1,14 @@
 
+# include "Convert.hpp"
+
 int main(int argc, char **argv)
 {
-	
+	if (argc == 1)
+	{
+
+	}
+	else
+		std::cout << "ERROR : This program just take one parameter" << std::endl;
+
 
 }
