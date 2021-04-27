d1e4bd5f3dcb13ddcbb1c7c2f899fbd8dcb29e34
Evrard Dessain <edessain@fu-r8-p4.s19.be>
2021-04-27 12:03:44 +0200

    Day06 ex00

---
+++ cpp06/ex00/Convert.cpp
@@ -0,0 +1,46 @@
+
+# include "Convert.hpp"
+
+/*
+** CONSTRUCTORS
+*/
+
+// DEFAULT
+Convert::Convert()
+{
+	
+}
+
+// COPY
+Convert::Convert(Convert const& copy)
+{
+	
+}
+
+/*
+** DESTRUCTOR
+*/
+
+Convert::~Convert()
+{
+	
+}
+
+/*
+** OPERATORS
+*/
+
+// ASSIGNATION
+Convert& Convert::operator=(Convert const& copy)
+{
+	if (this != &copy)
+	{
+		
+	}
+	return *this;
+}
+
+/*
+** MEMBER FUNCTIONS
+*/
+
---
+++ cpp06/ex00/Convert.hpp
@@ -0,0 +1,24 @@
+
+#ifndef CONVERT_HPP
+#define CONVERT_HPP
+
+# include <iostream>
+# include <string>
+# include <limits>
+# include <cmath>
+# include <cctype>
+
+class Convert
+{
+	public:
+		Convert();
+		Convert(Convert const& copy);
+		~Convert();
+		Convert& operator=(Convert const& copy);
+
+	private:
+
+
+};
+
+#endif
