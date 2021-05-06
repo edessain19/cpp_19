e17a73b9b6de452bf94da5a42ee39b1da8f0280d
Evrard Dessain <edessain@fu-r8-p4.s19.be>
2021-05-06 11:21:44 +0200

    Day01

--- cpp01/ex07/main.cpp
+++ cpp01/ex07/main.cpp
@@ -15,10 +15,7 @@ int main(int argc, char **argv)
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
 	std::ifstream ifs(argv[1]);
-	std::string	output;
-	std::string::size_type index = 0;
 	std::ofstream ofs(replace);
-
 	if (!ifs.is_open() || !ofs.is_open() || s1.empty() || s2.empty())
 	{
 		std::cerr << "Error" << std::endl;
@@ -26,13 +23,29 @@ int main(int argc, char **argv)
 		ofs.close();
 		return (1);
 	}
-	std::stringstream buffer;
-	buffer << ifs.rdbuf();
-	output = buffer.str();
 
-	while ((index = output.find(s1)) != std::string::npos)
-		output.replace(index, s1.length(), s2);
-	ofs << output;
+	std::string	output;
+	char    c;
+	int     i;
+	while (ifs.get(c))
+    {
+		i++;
+		output += c;
+		std::string str = s1;
+		str.resize(i);
+		if (output != str)
+		{
+			ofs << output;
+			output.erase();
+			i = 0;
+		}
+		else if (i == s1.size())
+		{
+			ofs << s2;
+			output.erase();
+			i = 0;
+		}
+    }
 	ifs.close();
 	ofs.close();
 	return (0);
