f22f5afd6b09dbdfeb321d4aa893191ae7b49926
Evrard Dessain <edessain@fu-r8-p4.s19.be>
2021-05-18 15:27:30 +0200

    intro

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -1,33 +1,39 @@
+
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
-template<typename T>
+# include <iostream>
+# include <string>
+# include <stdexcept>
+
+template <typename T>
 class Array
 {
-    public:
-        Array();
-        Array(unsigned int n);
-        ~Array(void);
-        Array(Array const& other);
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(Array const& copy);
+		~Array();
 
-    private:
-        unsigned int    _length;
-        T               *_array;
+		Array<T>& operator=(Array const& copy);
+		T & operator[](unsigned int const& i);
+		unsigned int	size(void) const;
+
+	private:
+		T				*_arr;
+		unsigned int	_n;
 
 };
 
 
-template<typename T>
-Array<T>::Array() : _array(0), _length(0) {}
+#endif
 
-template<typename T>
-Array<T>::Array(unsigned int n) : _array(new T[n]()), _length(n) {}
-
-template<typename T>
-Array<T>::~Array(void)
+template <typename T>
+Array<T>::Array(): _arr(0), _n(0) 
 {
-    if (this->_array)
-        delete this->_array;
 }
 
-#endif
+template<typename T>
+Array<T>::Array(unsigned int n): _arr(new T[n]()), _n(n) 
+{
+}
